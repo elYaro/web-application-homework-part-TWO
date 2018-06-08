e3f0edf04e7d5cc90a83f00fa4efed5a00dc100f
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2018-06-08 13:43:27 +0200

    base files to start the task and add new features

---
+++ data_manager.py
@@ -0,0 +1,118 @@
+import database_common
+
+
+@database_common.connection_handler
+def get_mentor_names_by_first_name(cursor, first_name):
+    cursor.execute("""
+                    SELECT first_name, last_name FROM mentors
+                    WHERE first_name = %(first_name)s ORDER BY first_name;
+                   """,
+                   {'first_name': first_name})
+    names = cursor.fetchall()
+    return names
+
+#TASK 1: 2 mentors names
+@database_common.connection_handler
+def get_mentor_2_names(cursor):
+    cursor.execute("""
+                    SELECT first_name, last_name FROM mentors
+                    ORDER BY last_name;
+                   """
+                   )
+    names = cursor.fetchall()
+    return names
+
+#TASK 2: mentor nick names
+@database_common.connection_handler
+def get_mentor_nick_names(cursor):
+    cursor.execute("""
+                    SELECT nick_name, first_name, last_name FROM mentors
+                    ORDER BY nick_name;
+                   """
+                   )
+    names = cursor.fetchall()
+    return names
+
+#TASK 3: find CAROL + her phone
+@database_common.connection_handler
+def get_find_CAROL(cursor):
+    cursor.execute("""
+                    SELECT CONCAT(first_name, ' ', last_name) AS full_name, phone_number 
+                    FROM applicants
+                    WHERE first_name = 'Carol';
+                   """
+                   )
+    names = cursor.fetchall()
+    return names
+
+#TASK 4: find her by email
+@database_common.connection_handler
+def get_find_her_by_email(cursor):
+    cursor.execute("""
+                    SELECT CONCAT (first_name, ' ', last_name) AS full_name, phone_number 
+                    FROM applicants
+                    WHERE email LIKE '%@adipiscingenimmi.edu';
+                   """
+                   )
+    names = cursor.fetchall()
+    return names
+
+#TASK 5: add new applicant
+@database_common.connection_handler
+def get_add_new_applicant(cursor):
+    cursor.execute("""
+                    INSERT INTO applicants (first_name, last_name, phone_number, email, application_code)
+                    VALUES ('Markus', 'Schaffarzyk', '003620/725-2666', 'djnovus@groovecoverage.com', 54823)
+                    ON CONFLICT DO NOTHING;
+                                      
+                    SELECT * FROM applicants 
+                    WHERE last_name = 'Schaffarzyk';               
+                   """
+                    )
+    names = cursor.fetchall()
+    return names
+
+#TASK 6: update Jemima Foreman's phone number
+@database_common.connection_handler
+def get_update_Jemima_Foremanadd(cursor):
+    cursor.execute("""
+                    UPDATE applicants
+                    SET phone_number =  '003670/223-7459'
+                    WHERE first_name = 'Jemima' AND last_name = 'Foreman';
+                                      
+                    SELECT * FROM applicants 
+                    WHERE first_name = 'Jemima' AND last_name = 'Foreman';               
+                   """
+                    )
+    names = cursor.fetchall()
+    return names
+
+#TASK 7: update Jemima Foreman's phone number
+@database_common.connection_handler
+def get_delete_applicant_by_email(cursor):
+    cursor.execute("""
+                    DELETE FROM applicants 
+                    WHERE email LIKE '%mauriseu.net';              
+                   """
+                    )
+    pass
+
+#Additional: show mentor table
+@database_common.connection_handler
+def get_show_mentor_table(cursor):
+    cursor.execute("""
+                    SELECT * FROM mentors;
+                   """
+                    )
+    names = cursor.fetchall()
+    return names
+
+#Additional: show applicant table
+@database_common.connection_handler
+def get_show_applicant_table(cursor):
+    cursor.execute("""
+                    SELECT * FROM applicants
+                   """
+                    )
+    names = cursor.fetchall()
+    return names
---
+++ main.py
@@ -0,0 +1,70 @@
+import data_manager
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/mentor-2-names')
+def mentor_2_names():
+    # We get back dictionaries here (for details check 'database_common.py')
+    mentor_2_names = data_manager.get_mentor_2_names()
+    return render_template('mentor_names.html', mentor_2_names=mentor_2_names)
+
+
+@app.route('/mentor_nick_names')
+def mentor_nick_names():
+    # We get back dictionaries here (for details check 'database_common.py')
+    mentor_nick_names = data_manager.get_mentor_nick_names()
+    return render_template('mentor_nick_names.html', mentor_nick_names=mentor_nick_names)
+
+
+@app.route('/find_CAROL')
+def find_CAROL():
+    # We get back dictionaries here (for details check 'database_common.py')
+    find_CAROL = data_manager.get_find_CAROL()
+    return render_template('find_CAROL.html', find_CAROL=find_CAROL)
+
+
+@app.route('/find_her_by_email')
+def find_her_by_email():
+    # We get back dictionaries here (for details check 'database_common.py')
+    find_her_by_email = data_manager.get_find_her_by_email()
+    return render_template('find_her_by_email.html', find_her_by_email=find_her_by_email)
+
+@app.route('/add_new_applicant')
+def add_new_applicant():
+    # We get back dictionaries here (for details check 'database_common.py')
+    add_new_applicant = data_manager.get_add_new_applicant()
+    return render_template('add_new_applicant_Markus.html', add_new_applicant=add_new_applicant)
+
+@app.route('/update_Jemima_Foremanadd')
+def update_Jemima_Foremanadd():
+    # We get back dictionaries here (for details check 'database_common.py')
+    update_Jemima_Foremanadd = data_manager.get_update_Jemima_Foremanadd()
+    return render_template('update_Jemima_Foremanadd.html', update_Jemima_Foremanadd=update_Jemima_Foremanadd)
+
+@app.route('/delete_applicant_by_email')
+def delete_applicant_by_email():
+    # We get back dictionaries here (for details check 'database_common.py')
+    delete_applicant_by_email = data_manager.get_delete_applicant_by_email()
+    return render_template('delete_applicant_by_email.html')
+
+@app.route('/show_mentor_table')
+def show_mentor_table():
+    # We get back dictionaries here (for details check 'database_common.py')
+    show_mentor_table = data_manager.get_show_mentor_table()
+    return render_template('show_mentors_table.html',show_mentor_table=show_mentor_table)    
+
+@app.route('/show_applicant_table')
+def show_applicant_table():
+    # We get back dictionaries here (for details check 'database_common.py')
+    show_applicant_table = data_manager.get_show_applicant_table()
+    return render_template('show_applicants_table.html', show_applicant_table=show_applicant_table)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
